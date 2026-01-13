a2791050378dccb4ddde579fe0476065f37c8d6a
tsshamanth <shamantht457@gmail.com>
2026-01-13 14:49:22 +0530

    APK

--- FrontEnd/flutter_application/android/app/build.gradle.kts
+++ FrontEnd/flutter_application/android/app/build.gradle.kts
@@ -18,8 +18,10 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_17)
+        }
     }
 
     defaultConfig {
